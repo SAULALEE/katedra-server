2ba2a60d17424fae2c74efe1c4c039e0ae29cfd1
SAULALEE <saulalejandro678@gmail.com>
2026-06-02 20:18:49 -0600

    chore: initial Spring Boot project setup with dependencies

---
+++ src/main/java/Katedra/Server/KatedraServerApplication.java
@@ -0,0 +1,13 @@
+package Katedra.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatedraServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatedraServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Katedra/Server/KatedraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Katedra.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatedraServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
